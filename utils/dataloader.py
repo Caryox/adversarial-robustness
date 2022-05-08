d729e33024d24b02f7581e940b8b53e0b5c58c6a
Kim-Kristin <582494@fom-net.de>
2022-05-08 22:58:17 +0200

    New: Dataloader Class and Param

---
+++ utils/dataloader.py
@@ -0,0 +1,3 @@
+# https://pytorch.org/tutorials/recipes/recipes/custom_dataset_transforms_loader.html
+# https://medium.com/analytics-vidhya/creating-a-custom-dataset-and-dataloader-in-pytorch-76f210a1df5d
+# https://medium.datadriveninvestor.com/how-to-custom-datasets-and-dataloaders-with-pytorch-e27f9e2a9009
