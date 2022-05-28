72ac98ee07c1a8c0239eabb7e9491f12ef03b1a2
Emily Tran <tran.ngoc.eb@gmail.com>
2022-05-28 14:33:53 +0200

    Added questions @bene

--- src/ensemble.py
+++ src/ensemble.py
@@ -5,6 +5,7 @@ Use the ensemble method as in the paper https://arxiv.org/pdf/2008.10400.pdf
 They also use majority voting, just no explaination, hence I need your help to understand what they do
 '''
 import numpy as np 
+#As I understand, we dont need this cuz we have param, right? 
 import argparse
 
 cnt = 1
@@ -14,11 +15,12 @@ curr = 10000
 p = argparse.ArgumentParser()
 p.add_argument("--kernel_size", default=5, type=int)
 args = p.parse_args()
-KERNEL_SIZE = args.kernel_size
+KERNEL_SIZE = args.kernel_size # could be 3, 5, 7, or 9
 
 for i in range(10):
     for j in range(i+1,10):
         for k in range(j+1,10):
+			#I understand that we need to load weights from modified models hier. but dont get the loop at all TT_TT
             w1 = np.loadtxt("../logs/modelM%d/wrong%03d.txt"%(KERNEL_SIZE, i)).astype(np.int)
             w2 = np.loadtxt("../logs/modelM%d/wrong%03d.txt"%(KERNEL_SIZE, j)).astype(np.int)
             w3 = np.loadtxt("../logs/modelM%d/wrong%03d.txt"%(KERNEL_SIZE, k)).astype(np.int)
@@ -27,6 +29,7 @@ for i in range(10):
             board[w1] += 1
             board[w2] += 1
             board[w3] += 1
+			#This part must be the majority voting part, which I dont understand. Still can use it, but it would be nice to know how it works
             board = board // 2
             curr = np.sum(board)
             if curr < best:
