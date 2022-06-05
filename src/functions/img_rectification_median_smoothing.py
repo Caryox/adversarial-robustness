64e44405317dd0220a43c42392b933c7c6952a49
Benedikt Hollmann <benedikt.hollmann@hotmail.de>
2022-06-05 14:51:08 +0200

    Created standalone .py file for bit reduction functionality, added synposis for functions

--- src/functions/img_rectification_median_smoothing.py
+++ src/functions/img_rectification_median_smoothing.py
@@ -1,3 +1,11 @@
+  # Smooth image by apllying median filter with kernel size (m*n). Use default kernel size of 2x2 as indicated by CW,
+  '''
+    Try to calculate median of given pixels inside of kernel window to normalize it's values.
+	The function can be used with batch or single image array by measuring its shape and thereby it's dimension.
+	The image data is casted into torch sensor because the scipy filter will change it's type otherwise to ndarray format.
+	Usage for batch: smoothed_batch = median_smoothing(original_batch)
+	Usage for single image: smoothed_image = median_smoothing(original_batch[index_of_image][0])
+  '''
 from scipy import ndimage
 import torch
 
