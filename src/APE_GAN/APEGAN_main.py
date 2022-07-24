f4ba316c8bba1c973a50b34b62293824edb9e9ca
daniel-knape <52594368+daniel-knape@users.noreply.github.com>
2022-07-24 18:31:00 +0200

    Update APEGAN_main.py

--- src/APE_GAN/APEGAN_main.py
+++ src/APE_GAN/APEGAN_main.py
@@ -5,20 +5,22 @@ import APEGAN_test
 import sys
 sys.path.append('./utils')
 sys.path.append('./src/Models')
-               
+sys.path.append('./src/functions')
+import upgraded_net_hook  
 import dataloader
 import device
+import F2D
 
-def main(train_loader, test_loader, device, input_channel=1, classes=10, eps=0.15):
+def main(train_loader, test_loader, device, resnet_path, apegan_path, epochs, eps=0.3):
     sample = next(iter(train_loader))
     input_channel = sample[0][0].shape[0]
-    #generate_adversarial_examples.gen_adv(train_loader , test_loader , device, input_channel, classes)
+    model = upgraded_net_hook.ResNet44(input_channel).to(device)
+    generate_adversarial_examples.gen_adv(train_loader , device, model, resnet_path, epochs, eps)
 
-    #APEGAN_train.APEGAN_Train(input_channel)
-    attack = ["FGSM", "PGD", "L2DeepFool", "C&W"]
+    APEGAN_train.APEGAN_Train(input_channel, epochs, apegan_path)
+    attack = ["FGSM"]
     for i in range(len(attack)):
-        APEGAN_test.APEGAN_Test("./checkpoint/test/2.tar", "./src/APE_GAN/cnn.tar", test_loader, device, attack[i], input_channel, classes, eps)
-    #APEGAN_test.APEGAN_Test("./checkpoint/test/2.tar", "./src/APE-GAN/cnn.tar",  test_loader, device, attack, input_channel, classes)
-
-#main(dataloader.train_dataloader , dataloader.test_dataloader, device.device )
-main(dataloader.train_dataloader_CIFAR10 , dataloader.test_dataloader_CIFAR10, device.device )
+        F2D.test_attack(test_loader, device, model, attack[i], resnet_path, apegan_path, input_channel, eps)
+    
+main(dataloader.train_dataloader , dataloader.test_dataloader, device.device, "./utils/resnet_model_100_MNIST.tar", "./utils/apegan_model_100_MNIST.tar", 100)
+#main(dataloader.train_dataloader_CIFAR10 , dataloader.test_dataloader_CIFAR10, device.device, "./utils/resnet_model_100_CIFAR10.tar", "./utils/apegan_model_100_CIFAR10.tar", 100)
