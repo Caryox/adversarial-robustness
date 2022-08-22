8921b895b95b7d59612586347efec4c217124927
daniel-knape <danielknape@outlook.com>
2022-08-22 19:55:24 +0200

    Final commit

--- src/APE_GAN/APEGAN_main.py
+++ src/APE_GAN/APEGAN_main.py
@@ -19,7 +19,7 @@ def main(train, train_loader, test_loader, device, resnet_path, apegan_path, res
         ResNet_train.resnet_train(train_loader, device, model, resnet_path, resnet_epochs)
         generate_adversarial_examples.gen_adv(train_loader , device, model, resnet_path, eps, input_channel)
         APEGAN_train.APEGAN_Train(input_channel, apegan_epochs, apegan_path)
-    attack = ["PGD"]
+    attack = ["FGSM", "PGD", "L2DeepFool", "C&W"]
     for i in range(len(attack)):
         if(attack[i] in ["L2DeepFool", "LinfDeepFool", "C&W"]):
             F2D.test_attack(test_loader, device, model, attack[i], resnet_path, apegan_path, input_channel, None)
@@ -28,4 +28,4 @@ def main(train, train_loader, test_loader, device, resnet_path, apegan_path, res
                 F2D.test_attack(test_loader, device, model, attack[i], resnet_path, apegan_path, input_channel, j)
     
 main(False, dataloader.train_dataloader , dataloader.test_dataloader, device.device, "./utils/resnet_model_10_MNIST.tar", "./utils/apegan_model_10_MNIST.tar", 100, 2, 0.15)
-#main(False, dataloader.train_dataloader_CIFAR10 , dataloader.test_dataloader_CIFAR10, device.device, "./utils/resnet_model_100_0_15_CIFAR10.tar", "./utils/apegan_model_100_0_15_CIFAR10.tar", 100, 30, 0.15)
+main(False, dataloader.train_dataloader_CIFAR10 , dataloader.test_dataloader_CIFAR10, device.device, "./utils/resnet_model_100_0_15_CIFAR10.tar", "./utils/apegan_model_100_0_15_CIFAR10.tar", 100, 30, 0.15)
